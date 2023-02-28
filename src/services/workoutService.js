279415fa650012c146972dc78457af1053b64df2
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-02-28 11:28:06 +0100

    added methods in workoutService.js that return nothing

---
+++ src/services/workoutService.js
@@ -0,0 +1,29 @@
+// Service layer:
+// Good practise to name service methods the same as the controller methods!
+const getAllWorkouts = () => {
+  return;
+};
+
+const getOneWorkout = () => {
+  return;
+};
+
+const createNewWorkout = () => {
+  return;
+};
+
+const updateOneWorkout = () => {
+  return;
+};
+
+const deleteOneWorkout = () => {
+  return;
+};
+
+module.exports = {
+  getAllWorkouts,
+  getOneWorkout,
+  createNewWorkout,
+  updateOneWorkout,
+  deleteOneWorkout,
+};

fe0779c41925f439adf8f485f70d92c0ab9a247f
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-02-28 16:50:36 +0100

    added logic to Workout.js

--- src/database/Workout.js
+++ src/database/Workout.js
@@ -6,7 +6,15 @@ const getAllWorkouts = () => {
   return DB.workouts;
 };
 
-const createdNewWorkout = (newWorkout) => {
+const getOneWorkout = (workoutId) => {
+  const workout = DB.workouts.find((workout) => workout.id === workoutId);
+  if (!workout) {
+    return;
+  }
+  return workout;
+};
+
+const createNewWorkout = (newWorkout) => {
   const isAlreadyAdded =
     DB.workouts.findIndex((workout) => workout.name === newWorkout.name) > -1;
   if (isAlreadyAdded) {
@@ -17,7 +25,38 @@ const createdNewWorkout = (newWorkout) => {
   return newWorkout;
 };
 
+const updateOneWorkout = (workoutId, changes) => {
+  const indexForUpdate = DB.workouts.findIndex(
+    (workout) => workout.id === workoutId
+  );
+  if (indexForUpdate === -1) {
+    return;
+  }
+  const updatedWorkout = {
+    ...DB.workouts[indexForUpdate],
+    ...changes,
+    updatedAt: new Date().toLocaleString('en-US', { timeZone: 'UTC' }),
+  };
+  DB.workouts[indexForUpdate] = updatedWorkout;
+  saveToDatabase(DB);
+  return updatedWorkout;
+};
+
+const deleteOneWorkout = (workoutId) => {
+  const indexForDeletion = DB.workouts.findIndex(
+    (workout) => workout.id === workoutId
+  );
+  if (indexForDeletion === -1) {
+    return;
+  }
+  DB.workouts.splice(indexForDeletion, 1);
+  saveToDatabase(DB);
+};
+
 module.exports = {
   getAllWorkouts,
-  createdNewWorkout,
+  createNewWorkout,
+  getOneWorkout,
+  updateOneWorkout,
+  deleteOneWorkout,
 };
